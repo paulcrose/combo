f4b31f46657439ff175ea8c6596498e3bf6c61a7
paulcrose <61593103+paulcrose@users.noreply.github.com>
2021-05-13 10:10:59 -0400

    ok

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-var username = "Bob";
-
-function changeVar(){
-    username = document.getElementById("userNameSlot").value;
-    document.getElementById('currentname').innerHTML = username;
-    localStorage.setItem("name", document.getElementById("userNameSlot").value);
-    console.log(username);
-} 
-
-function updateVar(){
-  document.getElementById('currentname').innerHTML = localStorage.getItem('name');
-}
-
 
 window.onload = () => {
   'use strict';
